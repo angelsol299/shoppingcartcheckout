4f71953f27e3d5323ac263001d780aa05dd38543
angelsol299 <angelsol299@hotmail.com>
2018-11-27 21:13:41 +0100

    estimated total element

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Grid } from "react-bootstrap";
 import Subtotal from "./components/Subtotal/Subtotal";
 import PickupSavins from "./components/PickupSavings/PickupSavings";
+import TaxesFees from "./components/TaxesFees/TaxesFess";
+import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal";
 
 import "./App.css";
 
@@ -11,7 +13,9 @@ class App extends Component {
 
     this.state = {
       total: 100,
-      PickupSavings: -3.85
+      PickupSavings: -3.85,
+      taxes: 0,
+      estimatedTotal: 0
     };
   }
   render() {
@@ -20,6 +24,9 @@ class App extends Component {
         <Grid className="purchase-card">
           <Subtotal price={this.state.total.toFixed(2)} />
           <PickupSavins price={this.state.PickupSavings} />
+          <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
         </Grid>
       </div>
     );
