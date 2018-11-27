6bc2d185140be6b8041d88bba471a3916461d6d6
angelsol299 <angelsol299@hotmail.com>
2018-11-27 20:53:58 +0100

    first checkout card

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import { Grid } from "react-bootstrap";
+import Subtotal from "./components/Subtotal/Subtotal";
 
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100
+    };
+  }
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
+      <div className="container">
+        <Grid className="purchase-card">
+          <Subtotal price={this.state.total.toFixed(2)} />
+        </Grid>
       </div>
     );
   }
