f6b362bca80f8bab10a16cf383aab62718d124ab
angelsol299 <angelsol299@hotmail.com>
2018-11-28 20:47:23 +0100

    redux to app.js

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -6,6 +6,8 @@ import TaxesFees from "./components/TaxesFees/TaxesFess";
 import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal";
 import ItemDetails from "./components/ItemDetails/ItemDetails";
 import PromoCode from "./components/PromoCode/PromoCode";
+import { connect } from "react-redux";
+import { handleChange } from "./actions/promoCodeActions";
 
 import "./App.css";
 
@@ -57,4 +59,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  promoCode: state.promoCode.value
+});
+
+export default connect(
+  mapStateToProps,
+  { handleChange }
+)(App);
